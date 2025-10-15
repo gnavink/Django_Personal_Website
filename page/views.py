d80554e9b9c899fe7dde40f9e4ec69fa027e9415
Navin Kumar <gnavink75@gmail.com>
2025-10-15 07:11:24 +0530

    added inheritance support in templates

--- page/views.py
+++ page/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Default home page view with hardcoded response
-def home_page_view(request):
-    return HttpResponse("Hello, World!")
+# def home_page_view(request):
+#     return HttpResponse("Hello, World!")
 
 # Default home page view using home.html
 # def home_page_view(request):
     # return render(request, 'home1.html')    
 
 # Default home page view with context & filters
-# def home_page_view(request):
-#     context = {
-#         "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
-#         "greeting": "THAnk you FOR visitING.",
-# }
-#     return render(request,'home2.html', context)
+def home_page_view(request):
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "THAnk you FOR visitING.",
+}
+    return render(request,'home3.html', context)
     
 
 # about page view with  hardcoded response
