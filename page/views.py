252f2fab346320b70bdec8df16cdf61802c82694
Navin Kumar <gnavink75@gmail.com>
2025-10-15 06:52:07 +0530

    class based view with context

--- page/views.py
+++ page/views.py
@@ -37,4 +37,10 @@ def home_page_view(request):
 
 # Class Based views
 class about_page_view(TemplateView):
-    template_name = "about1.html"
+    template_name = "about3.html"
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "555-555-5555"
+        return context
