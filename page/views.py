7adc3a610facde5f153dbe6e17c95e9dcc7f0009
Navin Kumar <gnavink75@gmail.com>
2025-10-15 05:58:01 +0530

    Added variations to test home and about page

--- page/views.py
+++ page/views.py
@@ -1,31 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Default home page view with hardcoded response
+def home_page_view(request):
+    return HttpResponse("Hello, World!")
 
-# def home_page_view(request):
-#     return HttpResponse("Hello, World!")
-
+# Default home page view using home.html
 # def home_page_view(request):
     # return render(request, 'home1.html')    
 
-def home_page_view(request):
-    context = { 
-        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
-        "greeting": "THAnk you FOR visitING.",
-}
-    return render(request,'home2.html', context)
+# Default home page view with context & filters
+# def home_page_view(request):
+#     context = {
+#         "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+#         "greeting": "THAnk you FOR visitING.",
+# }
+#     return render(request,'home2.html', context)
     
 
-# def about_page_view(request):
-#     return HttpResponse('About Page')
+# about page view with  hardcoded response
+def about_page_view(request):
+    return HttpResponse('About Page')
 
+# about page view using about.html
 # def about_page_view(request):
 #     return render(request, 'about1.html')
     
-
-def about_page_view(request):
-    context = {
-        'name': 'Navin',
-        'age': 50
-    }
-    return render(request, 'about2.html', context)
+# about page view using context
+# def about_page_view(request):
+#     context = {
+#         'name': 'Navin',
+#         'age': 50
+#     }
+#     return render(request, 'about2.html', context)
